97742daafd3f5cd8e14887ad59010359c87f78ae
Pavithra-69069 <108972686+Pavithra-69069@users.noreply.github.com>
2022-07-11 17:12:48 +0530

    Create DAY_7_PROGRAM_3.C

---
+++ DAY7/DAY_7_PROGRAM_3.C
@@ -0,0 +1,27 @@
+/**3.program to clear(0) bits CIS, CM2, CM1, CM0 in register “CMCON” if bits CHS3, CHS2, CHS1, CHS0 bits are set to 0x07 in register “ADCON0” **/
+
+
+PROGRAM:-
+
+#include <stdio.h>
+int main()
+{
+  //Declare variables.
+  int num1,num2;
+  printf("enter the values");
+  fflush(stdout);
+  //Ask inputs from the user.
+  scanf("%d%d",&num1,&num2);
+  //check the if condition for clearing  bit.
+  if((28 & num1) == 28)
+  {
+      //if condition is true excecute the below 
+      num2=num2&0xF0;
+      printf("%d",num2);
+  }
+  //if condition is false excecute else part.
+  else
+  printf(" not equal nothing");
+
+}
+===================================================================================================================================================
