252a7d327ea8a85d1015dfe0a96b0b28bc211d5b
Pavithra-69069 <108972686+Pavithra-69069@users.noreply.github.com>
2022-07-11 16:41:43 +0530

    Create DAY_2_PROGRAM_6.C

---
+++ DAY2/DAY_2_PROGRAM_6.C
@@ -0,0 +1,13 @@
+/**6)Write a program to convert Celsius form of temperature into Fahrenheit and vice versa.**/
+
+#include <stdio.h>
+int main()
+{
+float f,c,t;
+scanf("%f",&c);
+f=c*9/5+32;
+c=5/9*(f-32);
+printf("convert Celsius to Fahrenheit %f\n",f);
+printf(" convert Fahrenheit to Celsius %f\n",c);
+}
+
