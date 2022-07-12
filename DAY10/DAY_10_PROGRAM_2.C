67457654048c0075dff7db5d375d9ffaf1746f44
Pavithra-69069 <108972686+Pavithra-69069@users.noreply.github.com>
2022-07-12 14:04:41 +0530

    Update DAY_10_PROGRAM_2.C

--- DAY10/DAY_10_PROGRAM_2.C
+++ DAY10/DAY_10_PROGRAM_2.C
@@ -4,33 +4,26 @@ signed int G_Str_SteeringAngle_sint = -60;
 
 
 unsigned char G_Msg_SteeringInformation_Byte[3u];" **/
-
-#include <stdio.h>
-#include <stdlib.h>
-int main()
-{
-    int reg0,reg1,reg2;
-    signed SteeringAngle_sint = -60;
-unsigned char SteeringInformation_Byte[3u];
-
-reg0= SteeringAngle_sint|(0x00);
-printf("SteeringAngle_sint reg0:");
-printf("%x %x ",((reg0>>7)&1),(reg0>>6)&1);
-printf("\n");
-
-reg1=SteeringAngle_sint|(0x00);
-printf("SteeringAngle_sint reg1:");
-for(int i=7;i>=0;i--)
-{
-    printf("%x ",(reg1>>i)&1);
+#include<stdio.h>
+int main(){
+  // 1111 1100 0100
+    signed int G_Str_SteeringAngle_sint = -60;  
+    for(int i=11;i>=0;i--){
+      //printing decimal to binary form  
+        printf("%x ",(G_Str_SteeringAngle_sint>>i)&1);      
+       }
+   //assigning array size 3 
+    unsigned char G_Msg_SteeringInformation_Byte[3u]={0x00,0x00,0x00}; 
+  //assigning 10 and 11 bits in an array of 0th element
+    G_Msg_SteeringInformation_Byte[0]=((G_Str_SteeringAngle_sint>>0)&~(1<<2)); 
+   //assigning 2 to 9 bits in an array of 1st element  
+    G_Msg_SteeringInformation_Byte[1]=(G_Str_SteeringAngle_sint>>2); 
+  //assigning 0 and 1st bit in an array of 2nd element
+    G_Msg_SteeringInformation_Byte[2]=(G_Str_SteeringAngle_sint<<6);            
+    for(int i=0;i<3;i++){
+       printf("\nG_Msg_SteeringInformation_Byte[%d]:\n",i);
+        for(int j=7;j>=0;j--)
+          //printing array element of size 3
+            printf("%d ",(G_Msg_SteeringInformation_Byte[i]>>j)&1);             
+       }
 }
-    reg2= SteeringAngle_sint|(0x00);
-    printf("\n");
-printf("SteeringAngle_sint reg0:");
-printf("%x %x ",((reg0>>1)&1),(reg0>>2)&1);
-printf("\n");
-}
-
-
-
-
